928836f64cfa643f5aa2007a8aa7342f1f5d44c6
Wiktor Gębski <wiktor07tutu@gmail.com>
2023-05-15 17:36:20 +0200

    Wprowadzanie liczby

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,25 +3,34 @@
 Plansza = [' -',' -',' -',
         ' -',' -',' -',
         ' -',' -',' -']
-ObecnyGracz = 'X'
+ObecnyGracz = ' X'
 Zwyciezca = None
-TrwajacaGra = True
+TrwaGra = True
 
 #Plansza do gry
 
 def PlanszaDoGry(Plansza):
     print('│' + Plansza[0] + ' │' + Plansza[1] + ' │' + Plansza[2] + ' │')
     print('├───┼───┼───┤')
-    print('│' + Plansza[0] + ' │' + Plansza[1] + ' │' + Plansza[2] + ' │')
+    print('│' + Plansza[3] + ' │' + Plansza[4] + ' │' + Plansza[5] + ' │')
     print('├───┼───┼───┤')
-    print('│' + Plansza[0] + ' │' + Plansza[1] + ' │' + Plansza[2] + ' │') 
-     
-PlanszaDoGry(Plansza)
+    print('│' + Plansza[6] + ' │' + Plansza[7] + ' │' + Plansza[8] + ' │') 
 
-#Wpisanie danych przez gracza
+#Wpisanie koordynatow przez gracza
+
+def WprowadzLiczbe(Plansza):
+    i = int(input('Wprowadz liczbe w zakresie 1-9: '))
+    if i >= 1 and i <= 9 and Plansza[i-1] == ' -':
+        Plansza[i-1] = ObecnyGracz
+    else:
+        print('Podana liczba nie jest w zakresie od 1-9 lub podana liczba jest juz zajeta')
 
 #Sprawdzenie czy dany ruch daje zwyciestwo lub remis
 
 #Zmiana gracza
 
 #Sprawdzenie czy dany ruch daje zwyciestwo lub remis
+
+while TrwaGra:
+    PlanszaDoGry(Plansza)
+    WprowadzLiczbe(Plansza)
