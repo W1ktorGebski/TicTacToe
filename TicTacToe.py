d8d9f16c0d1b9c0e76cd67af704c48fdef8bfc89
Wiktor Gębski <wiktor07tutu@gmail.com>
2023-05-15 17:20:17 +0200

    Plansza

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,27 @@
 #Tic Tac Toe
+
+Plansza = [' -',' -',' -',
+        ' -',' -',' -',
+        ' -',' -',' -']
+ObecnyGracz = 'X'
+Zwyciezca = None
+TrwajacaGra = True
+
+#Plansza do gry
+
+def PlanszaDoGry(Plansza):
+    print('│' + Plansza[0] + ' │' + Plansza[1] + ' │' + Plansza[2] + ' │')
+    print('├───┼───┼───┤')
+    print('│' + Plansza[0] + ' │' + Plansza[1] + ' │' + Plansza[2] + ' │')
+    print('├───┼───┼───┤')
+    print('│' + Plansza[0] + ' │' + Plansza[1] + ' │' + Plansza[2] + ' │') 
+     
+PlanszaDoGry(Plansza)
+
+#Wpisanie danych przez gracza
+
+#Sprawdzenie czy dany ruch daje zwyciestwo lub remis
+
+#Zmiana gracza
+
+#Sprawdzenie czy dany ruch daje zwyciestwo lub remis
